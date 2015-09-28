138a59dedb7c1cfa9f57175ee3de75e324aae50f
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2015-09-28 13:41:50 +0300

    added data.php content, login and creating user to functions.php

--- data.php
+++ data.php
@@ -1,1 +1,22 @@
-asd
+<?php
+    require_once("functions.php");
+	
+	//Kui kasutaja ei ole sisse logitud, suuna teisele lehele
+	//Kontrollin kas sessiooni muutuja on olemas
+	
+	if(!isset($_SESSION['logged_in_user_id'])) {
+		header("Location: login.php");
+	}
+
+	//aadressireale tekkis ?logout=1
+	if(isset($_GET["logout"])) {
+		//kustutame sessiooni muutujad
+		session_destroy();
+		header("Location: login.php");
+	}
+
+
+?>
+
+Tere, <?=$_SESSION['logged_in_user_email']?>
+<a href="?logout=1">Logi välja</a>
